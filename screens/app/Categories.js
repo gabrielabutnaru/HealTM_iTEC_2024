6a2ef455a601c2ac62d7d992664e088ff0e6cad1
hentesandreea <deeahe@gmail.com>
2024-04-05 19:07:50 +0300

    Nav sk

---
+++ screens/app/Categories.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Categories = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Here will be doctors options</Text>
+    </View>
+  );
+};
+
+export default Categories;
---
+++ screens/app/Home.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Tags with medical specialities</Text>
+    </View>
+  );
+};
+
+export default Home;
